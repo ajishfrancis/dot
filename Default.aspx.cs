d890d4b571820c5e6577c748bd3552e12bec2bcc
ajish.chovallur <ajish.chovallur@atos.net>
2017-01-13 18:04:45 +0530

    ereq

---
+++ Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("hello world");
+        }
+    }
+}
